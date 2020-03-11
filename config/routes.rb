fd4ab7b6b4fa3c24c43d2bc0a68fa8f38989f07e
sarafotros <sarafotros@gmail.com>
2020-03-11 20:05:25 +0000

    Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
+      resources :users
+      post "/login", to: "users#login"
+      post "/create", to: "users#create"
+      get "/validate", to: "users#validate"
+    
 end
