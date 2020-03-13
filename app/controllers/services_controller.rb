da740b6feae8683056806ac7c46aa9b75b8cf568
sarafotros <sarafotros@gmail.com>
2020-03-13 12:51:05 +0000

    services routes

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,9 @@
+class ServicesController < ApplicationController
+        def index 
+        render json: Service.all
+    end
+
+    def show 
+       render json: Service.find(params[:id])
+    end
+end
