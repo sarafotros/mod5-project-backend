182f1bc504f6e1d1ca4ba18a2d20e183e4c03e5f
sarafotros <sarafotros@gmail.com>
2020-03-15 00:17:17 +0000

    seed data sevice name errors  resolved

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,11 +15,11 @@ u2 = User.create(name:"Toli", username:"Benson", password:"123", phone:"12312312
 u3 = User.create(name:"Sara", username:"Fotros", password:"123", phone:"123123123", admin?:true)
 
 
-Service.create(name:"Coneversion/Building", category:"Builder", price:6000 ,description:"Home builders are involved in the physical labor aspect of construction, such as framing houses, installing sheathing and subfloors, building foundations, and installing roofing.", image:"https://picsum.photos/200
+Service.create(name:"Coneversion or Building", category:"Builder", price:6000 ,description:"Home builders are involved in the physical labor aspect of construction, such as framing houses, installing sheathing and subfloors, building foundations, and installing roofing.", image:"https://picsum.photos/200
 " )
-Service.create(name:" House/Conservatory Improvement", category:"Builder", price: 7000 ,description:"Perform finishing work, such as flooring, trim work, and installing doors and windows ", image:"https://picsum.photos/200
+Service.create(name:"House or Conservatory Improvement", category:"Builder", price: 7000 ,description:"Perform finishing work, such as flooring, trim work, and installing doors and windows ", image:"https://picsum.photos/200
 " )
-Service.create(name:"Paintin", category:"Renovating", price: 1600 ,description:"Preparing painting surfaces by washing walls, repairing holes, or removing old paint.to improve the appearance of a building and to protect it from damage by water, corrosion, insects and mold", image:"https://picsum.photos/200
+Service.create(name:"Painting", category:"Renovating", price: 1600 ,description:"Preparing painting surfaces by washing walls, repairing holes, or removing old paint.to improve the appearance of a building and to protect it from damage by water, corrosion, insects and mold", image:"https://picsum.photos/200
 " )
 Service.create(name:"Decorating", category:"Renovating", price:4200 ,description:"meet with the client to determine style, budget and needs. The decorator then plans the space, often using a CAD program, and determines color palette, furniture options and interior layout. Additional considerations include structural changes and building codes. Once the design is approved, the decorator begins shopping for the furniture, window treatments and artwork. The decorator supervises the installation of the design and ensures the satisfaction of their client.", image:"https://picsum.photos/200
 " )
